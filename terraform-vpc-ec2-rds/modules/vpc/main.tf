9685a982c7dede72799d245547d38ae2944b0f43
Ketan Kakde <ketankakde@outlook.com>
2026-06-08 12:00:14 +0530

    Fix cidr_block syntax in VPC and subnets

--- terraform-vpc-ec2-rds/modules/vpc/main.tf
+++ terraform-vpc-ec2-rds/modules/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = ["10.0.0.0/16"]
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "demo-vpc"
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = ["10.0.1.0/24"]
+  cidr_block = "10.0.1.0/24"
 
   map_public_ip_on_launch = true
 
@@ -19,7 +19,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = ["10.0.2.0/24"]
+  cidr_block = "10.0.2.0/24"
 
   tags = {
     Name = "pri-subnet"
@@ -34,7 +34,7 @@ resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = ["0.0.0.0/0"]
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 }
