e661464eb8fd4e8ca45db6dbddda4b80cabed53b
Ketan Kakde <ketankakde@outlook.com>
2026-06-08 20:55:16 +0530

    Add RDS variables for database configuration

--- terraform-vpc-ec2-rds/modules/rds/variables.tf
+++ terraform-vpc-ec2-rds/modules/rds/variables.tf
@@ -1,3 +1,37 @@
+variable "identifier" {
+ default = "mysql-db"
+}
+
+variable "engine" {
+ default = "mysql" 
+}
+
+variable "engine_version" {
+ default = "8.0"
+ type = number
+}
+
+variable "instance_class" {
+ default = "db.t4g.micro"
+}
+
+variable "allocated_storage" {
+ default = 20
+ type = number
+}
+
+variable "db_name" {
+ default = "student_registration
+}
+
+variable "username" {
+ default = "root"
+}
+
+variable "password" {
+ default = "password123"
+}
+
 variable "vpc_id" {}
 variable "private_subnet_id" {}
 variable "private_2_id" {}
