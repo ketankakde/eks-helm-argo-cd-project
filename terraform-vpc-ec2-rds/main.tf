e9107bb177c8182300b6bd6db37212455d0d33f5
Ketan Kakde <ketankakde@outlook.com>
2026-06-08 11:29:22 +0530

    Add IAM module integration in main.tf

--- terraform-vpc-ec2-rds/main.tf
+++ terraform-vpc-ec2-rds/main.tf
@@ -6,11 +6,16 @@ module "vpc" {
   private_subnet_cidr = var.private_subnet_cidr
 }
 
+module "iam" {
+  source = "./modules/iam"
+}
+
 module "ec2" {
   source = "./modules/ec2"
 
   vpc_id        = module.vpc.vpc_id
   subnet_id     = module.vpc.public_subnet_id
+  instance_profile_name = module.iam.instance_profile_name
 }
 
 module "rds" {
