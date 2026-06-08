b8320e11cf251e5bf2d66456166d5178c2be3f94
Ketan Kakde <ketankakde@outlook.com>
2026-06-08 13:11:07 +0530

    Install AWS CLI and unzip in install_docker.sh

--- terraform-vpc-ec2-rds/modules/ec2/install_docker.sh
+++ terraform-vpc-ec2-rds/modules/ec2/install_docker.sh
@@ -3,3 +3,7 @@ sudo apt update
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
+sudo apt install unzip -y
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install
