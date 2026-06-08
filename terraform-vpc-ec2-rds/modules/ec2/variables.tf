e35ca6d4d0aaf55cdc4940dc9ec6e99a8d0dc1dd
Ketan Kakde <ketankakde@outlook.com>
2026-06-08 21:20:21 +0530

    Add EC2 variables for AMI, instance type, and key name

--- terraform-vpc-ec2-rds/modules/ec2/variables.tf
+++ terraform-vpc-ec2-rds/modules/ec2/variables.tf
@@ -1,3 +1,15 @@
+variable "ami" {
+ default = "ami-091138d0f0d41ff90"
+}
+
+variable "instance_type" {
+ default = "t3.micro"
+}
+
+variable "key_name" {
+ default = "new-key"
+}
+
 variable "vpc_id" {}
 variable "subnet_id" {}
 variable "instance_profile_name" {}
