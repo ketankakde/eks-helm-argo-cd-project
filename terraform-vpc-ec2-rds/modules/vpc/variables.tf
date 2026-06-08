1571486d3159c77e9d5a27c43ec188be18ce9c1e
Ketan Kakde <ketankakde@outlook.com>
2026-06-08 21:14:57 +0530

    Add VPC variables for CIDR blocks and availability zones

---
+++ terraform-vpc-ec2-rds/modules/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "cidr_block" {
+ default = "10.0.0.0/16"
+}
+
+variable "pub_cidr" {
+ default = "10.0.1.0/24"
+}
+
+vaiable "az-1" {
+ default = "us-east-1a"
+}
+
+variable "pri_cidr" {
+ default = "10.0.2.0/24"
+}
+
+variable "az-2" {
+ default = "us-east-1a"
+}
+
+variable "pri_2_cidr" {
+ default = "10.0.3.0/24"
+}
+
+variable "az-3 {
+ default = "us-east-1b"
+}
