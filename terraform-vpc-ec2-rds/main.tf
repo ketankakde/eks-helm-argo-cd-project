7706407aff40137321b0ca03a567ddf98b9e70d0
Ketan Kakde <ketankakde@outlook.com>
2026-06-08 11:58:20 +0530

    Remove CIDR variable assignments in main.tf
    Removed CIDR variable assignments from VPC module.

--- terraform-vpc-ec2-rds/main.tf
+++ terraform-vpc-ec2-rds/main.tf
@@ -1,9 +1,5 @@
 module "vpc" {
   source = "./modules/vpc"
-
-  vpc_cidr            = var.vpc_cidr
-  public_subnet_cidr  = var.public_subnet_cidr
-  private_subnet_cidr = var.private_subnet_cidr
 }
 
 module "iam" {
