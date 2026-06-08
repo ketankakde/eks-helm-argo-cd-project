153cf835a4dff1a8603b6c9dfe4d1be12cabac2d
Ketan Kakde <ketankakde@outlook.com>
2026-06-08 12:07:26 +0530

    Add second private subnet in VPC module

--- terraform-vpc-ec2-rds/modules/vpc/main.tf
+++ terraform-vpc-ec2-rds/modules/vpc/main.tf
@@ -20,10 +20,17 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "pri-subnet"
   }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
 }
 
 resource "aws_internet_gateway" "igw" {
